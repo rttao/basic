2a86964f93bf110d849e3fe6289d237f52d488e5
Eric <rttao@163.com>
2016-01-01 20:28:38 +0800

    string practise

--- control.py
+++ control.py
@@ -4,12 +4,27 @@ while b<1000:
     print(a)
     c=a+b;a=b;b=c
 
-b=701
-# if
-if b >= 500:
-    print("LARGE")
-elif b >= 700:
-    print("Very LARGE")
-else:
+# if, python没有switch case
+if b <= 500:
     print("small")
+elif b <= 700:
+    print("LARGE")
+else:
+    print("Very large")
 
+if a<=500:
+    if b<=500:
+        b += a
+    elif b <= 700:
+        b = b-a
+else:
+    b = a*b
+print(a,b)
+
+# for 循环
+count = range(100)
+x = 0
+for i in count:
+  x += i
+
+print(x)
